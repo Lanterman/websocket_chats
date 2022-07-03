2405913b04b5ea55ee45ade0ba572d840e1d1a5a
Lanterman <splen1011@mail.ru>
2022-07-03 13:19:26 +0300

    replace functions to classes in views file, update main_page.html and main_page.css

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from main import views
 
 urlpatterns = [
-    path('', views.main_page, name="main_page"),
-    path('chat/<slug:chat_slug>/', views.chat_detail, name="chat_detail"),
+    path('', views.ListMainPageView.as_view(), name="main_page"),
+    path('chat/<slug:chat_slug>/', views.ChatDetailView.as_view(), name="chat_detail"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,26 +1,61 @@
 from django.db.models import Prefetch
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 
 from main.models import Chat, Message
 
 
-def main_page(request):
+# def main_page(request):
+#     """Main page"""
+#
+#     my_chats = Chat.objects.filter(users=request.user.id).prefetch_related(Prefetch(
+#         'chat_messages',
+#         queryset=Message.objects.exclude(is_read=request.user.id),
+#         to_attr="set_messages"
+#     ))
+#     chats_without_me = Chat.objects.exclude(users=request.user.id)
+#     context = {"my_chats": my_chats, "chats_without_me": chats_without_me}
+#     return render(request, 'main/main_page.html', context)
+
+
+# def chat_detail(request, chat_slug):
+#     """Chat detail"""
+#
+#     chat = Chat.objects.get(slug=chat_slug)
+#     chat_messages = Message.objects.filter(chat_id=chat.id).select_related("owner_id").prefetch_related("is_read")
+#     context = {"chat": chat, "chat_messages": chat_messages}
+#     return render(request, "main/chat_detail.html", context)
+
+
+class ListMainPageView(ListView):
     """Main page"""
 
-    my_chats = Chat.objects.filter(users=request.user.id).prefetch_related(Prefetch(
-        'chat_messages',
-        queryset=Message.objects.exclude(is_read=request.user.id),
-        to_attr="set_messages"
-    ))
-    chats_without_me = Chat.objects.exclude(users=request.user.id)
-    context = {"my_chats": my_chats, "chats_without_me": chats_without_me}
-    return render(request, 'main/main_page.html', context)
+    context_object_name = "my_chats"
+    template_name = "main/main_page.html"
+
+    def get_queryset(self):
+        return Chat.objects.filter(users=self.request.user.id).prefetch_related(Prefetch(
+            'chat_messages',
+            queryset=Message.objects.exclude(is_read=self.request.user.id),
+            to_attr="set_messages"
+        ))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['chats_without_me'] = Chat.objects.exclude(users=self.request.user.id).select_related("owner_id")[:5]
+        return context
 
 
-def chat_detail(request, chat_slug):
+class ChatDetailView(DetailView):
     """Chat detail"""
 
-    chat = Chat.objects.get(slug=chat_slug)
-    chat_messages = Message.objects.filter(chat_id=chat.id).select_related("owner_id")
-    context = {"chat": chat, "chat_messages": chat_messages}
-    return render(request, "main/chat_detail.html", context)
+    context_object_name = "chat"
+    slug_url_kwarg = "chat_slug"
+    template_name = "main/chat_detail.html"
+    queryset = Chat.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['chat_messages'] = Message.objects.filter(chat_id=self.object.id).select_related(
+            "owner_id").prefetch_related("is_read")
+        return context

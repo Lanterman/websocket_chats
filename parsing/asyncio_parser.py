d4e78e7a0de89dff8a78a314d2513a531db20577
Lanterman <splen1011@mail.ru>
2022-12-18 15:43:51 +0300

    aiohttp replace to httpx, refactoring asuncio parser, added log decorator for endpoint, done output data with chats without user, updated output data for user info, writed function for collect dict with all information

--- parsing/asyncio_parser.py
+++ parsing/asyncio_parser.py
@@ -2,11 +2,11 @@ import os
 import json
 import random
 import string
+import httpx
 import asyncio
 import fake_useragent
 import logging as log
 
-from requests.sessions import Session
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup as BS
 
@@ -29,11 +29,42 @@ class Settings:
         }
 
 
+
+def info_log(func):
+    """Decorator informing about the progress of the parser"""
+
+    async def logs_output():
+        log.info(msg="Started the asyncio parser")
+        log.info(msg="Data is written to the data_by_asyncio_parser.json file")
+        await func()
+        log.info(msg="Finished the selenium parser")
+        log.info(msg="Check the data_by_asyncio_parser.json file")
+
+    return logs_output
+
+
 def write_to_json_file(data: dict) -> None:
     """Write to json file"""
 
     with open(file="data_by_asyncio_parser.json", mode="w") as file:
         json.dump(obj=data, fp=file, indent=4, ensure_ascii=False)
+
+
+def collect_dict(user_info: dict, chat_info_list: list) -> dict:
+    """Collects all information in dictionary"""
+
+    number_of_chats = len(chat_info_list)
+    public_chats = sum(1 for index, _ in enumerate(chat_info_list) if chat_info_list[index]["is_password"] == "Нет")
+    private_chats = number_of_chats - public_chats
+
+    output_data = {
+        "number_of_chats": number_of_chats,
+        "number_of_public_chats": public_chats,
+        "number_of_private_chats": private_chats,
+        "user_info": user_info,
+        "chat_without_user": chat_info_list
+    }
+    return output_data
 
 
 def set_headers() -> dict:
@@ -50,19 +81,46 @@ def create_random_username(length: int = 12) -> str:
     return "".join(random.choice(string.ascii_letters) for _ in range(length))
 
 
-def authorization(html: BS, session: Session) -> BS:
+def create_dict_with_user_info(html: BS) -> dict:
+    """Create dict with user info from user profile"""
+
+    user_info = {
+        "username": html.find(name="i", attrs={"id": "username"}).text,
+        "first_name": html.find(name="i", attrs={"id": "first_name"}).text,
+        "last_name": html.find(name="i", attrs={"id": "last_name"}).text,
+        "email": html.find(name="i", attrs={"id": "email"}).text,
+        "is_superuser": html.find(name="i", attrs={"id": "is_superuser"}).text,
+    }
+
+    return user_info
+
+
+def get_chat_users(data: str, chat_info: dict) -> dict:
+    """Parses chat user"""
+
+    html = BS(markup=data, features="html.parser")
+    list_user_html = html.select("#list_users > p > .redirect")
+
+    for user in list_user_html:
+        user_info = {"username": user.text, "link": f"{settings.base_url}{user.attrs['href']}"}
+        chat_info["chat_users"] = user_info
+
+    return chat_info
+
+
+async def authorization(html: BS, session: httpx.AsyncClient) -> BS:
     """Authentication and authorization user"""
 
     scrf = html.find(name="input", attrs={"name": "csrfmiddlewaretoken"}).get("value")
     settings.login_information["csrfmiddlewaretoken"] = scrf
 
-    request = session.post(url=settings.login_url, data=settings.login_information)
+    request = await session.post(url=settings.login_url, data=settings.login_information)
     html = BS(request.text, "html.parser")
 
     return html
 
 
-def registration(html: BS, session: Session) -> BS:
+async def registration(html: BS, session: httpx.AsyncClient) -> BS:
     """Registration user"""
 
     scrf = html.find(name="input", attrs={"name": "csrfmiddlewaretoken"}).get("value")
@@ -70,58 +128,118 @@ def registration(html: BS, session: Session) -> BS:
     settings.register_information["username"] = create_random_username()
     settings.register_information["csrfmiddlewaretoken"] = scrf
 
-    request = session.post(url=settings.register_url, data=settings.register_information)
+    request = await session.post(url=settings.register_url, data=settings.register_information)
     html = BS(request.text, "html.parser")
 
     return html
 
 
-def get_user_info(html: BS, session: Session) -> dict:
+async def fetch(link:str, session: httpx.AsyncClient) -> httpx.Response:
+    """Makes request"""
+
+    request = await session.request(method="get", url=link)
+    return request
+
+
+async def get_user_info(html: BS, session: httpx.AsyncClient) -> dict:
     """Get user info"""
 
     link_to_user_profile = html.find(name="a", attrs={"class": "redirect"}).attrs["href"]
-
-    request = session.get(url=f"{settings.base_url}{link_to_user_profile}")
+    request = await fetch(link_to_user_profile, session)
     html = BS(markup=request.text, features="html.parser")
 
-    user_info = {
-        "username": html.find(name="i", attrs={"id": "username"}),
-        "first_name": html.find(name="i", attrs={"id": "first_name"}),
-        "last_name": html.find(name="i", attrs={"id": "last_name"}),
-        "email": html.find(name="i", attrs={"id": "email"}),
-        "is_superuser": html.find(name="i", attrs={"id": "is_superuser"}),
-    }
+    user_info = create_dict_with_user_info(html)
 
     return user_info
 
 
+async def get_info_about_owner(session: httpx.AsyncClient, chat_owner_link: str) -> dict:
+    """Makes request and parses info about chat owner"""
 
-async def create_task_list(html: BS, session: Session) -> tuple:
-    """Create task lisk for concurrent execution of queries and analysis of their data"""
+    response = await fetch(chat_owner_link, session)
+
+    html = BS(markup=response.text, features="html.parser")
+
+    return create_dict_with_user_info(html)
 
 
+async def parse_chat_without_user(session: httpx.AsyncClient, chat_html) -> dict:
+    """Parses chats without user from main page"""
+
+    chat_name = chat_html.select(".chat_name > .redirect")[0]
+    chat_owner_link = chat_html.select(".chat_owner_name > .redirect")[0].attrs["href"]
+    is_password = chat_html.select(".is_password > i")[0].text
+
+    chat_info = {
+        "chat_name": {"name": chat_name.text, "link_to_chat": f"{settings.base_url}{chat_name.attrs['href']}"},
+        "chat_owner": await get_info_about_owner(session, chat_owner_link),
+        "chat_users": "",
+        "is_password": is_password
+    }
+
+    return chat_info
 
 
+async def chat_analysis(session: httpx.AsyncClient, chat_info: dict) -> dict:
+    """Makes request and parses response data"""
+
+    if chat_info["is_password"] == "Нет":
+        link_to_chat = chat_info["chat_name"]["link_to_chat"]
+
+        response = await fetch(link_to_chat, session)
+        chat_info = get_chat_users(response.text, chat_info)
+
+    return chat_info
+
+
+async def create_task_list_for_parsing_chats(session: httpx.AsyncClient, html: BS) -> asyncio.Future:
+    """Create task list for parsing chats without user from main page"""
+
+    task_list = []
+    chats_html = html.find_all(name="div", attrs={"class": "chat_without_me"})
+
+    for chat_html in chats_html:
+        task = asyncio.create_task(parse_chat_without_user(session, chat_html))
+        task_list.append(task)
+
+    return asyncio.gather(*task_list)
+
+
+async def create_task_list_for_parsing_open_chats(session: httpx.AsyncClient, chats_info: list) -> asyncio.Future:
+    """Create task lisk for parsing open chats"""
+
+    task_list = []
+
+    for chat_info in chats_info:
+        task = asyncio.create_task(chat_analysis(session, chat_info))
+        task_list.append(task)
+
+    return asyncio.gather(*task_list)
+
+
+@info_log
 async def main() -> None:
     """Endpoint"""
 
-    with Session() as session:
-        session.headers = set_headers()
-        request = session.get(settings.base_url)
+    async with httpx.AsyncClient(base_url=settings.base_url, follow_redirects=True) as session:
+        session.headers.update(set_headers())
+        request = await session.get(url="/")
         html = BS(request.text, "html.parser")
 
-        auth_html = authorization(html, session)
+        auth_html = await authorization(html, session)
 
         if auth_html.find(name="h1", attrs={"id": "title"}) is None:
-            auth_html = registration(html, session)
+            auth_html = await registration(html, session)
 
-        user_info = get_user_info(auth_html, session)  # write to json
-        print(user_info)
+        user_info = await get_user_info(auth_html, session)
 
-        # output_data = await create_task_list(auth_html, session)
+        chats_without_user = await create_task_list_for_parsing_chats(session, auth_html)
 
-        # write_to_json_file(output_data)
+        task_list = await create_task_list_for_parsing_open_chats(session, await chats_without_user)
 
+        output_data = collect_dict(user_info, await task_list)
+
+        write_to_json_file(output_data)
 
 
 if __name__ == "__main__":
